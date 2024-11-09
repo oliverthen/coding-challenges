52a690b00a1df653db7a25294296b97d5945d953
Oliver Then <oli171dr@gmail.com>
2024-11-09 13:32:40 -0500

    By using Click and OS module, able to output number of bytes in a file

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -1,10 +1,16 @@
 import click
+import os
+
 
 @click.command()
-@click.option('--c', help='Count the number of bytes in a file')
-@click.argument('file')
-def hello():
-    click.echo("Hello World")
+@click.argument("filename", required=True)
+@click.option("-c", is_flag=True, help="Count the number of bytes in a file")
+def cli(filename, c):
+    if c:
+        cur_dir = os.getcwd()
+        cur_path = os.path.join(cur_dir, filename)
+        num_bytes = os.path.getsize(cur_path)
+        click.echo(f"{num_bytes} {filename}")
 
 if __name__ == "__main__":
-    hello()
+    cli()
