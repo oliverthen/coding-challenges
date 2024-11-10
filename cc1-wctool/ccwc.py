8a112567458b0a41d99f43a3b33b358e1d84bbdf
Oliver Then <oli171dr@gmail.com>
2024-11-10 14:31:28 -0500

    Added command option -l that outputs number of lines in file which works as intended

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -5,12 +5,19 @@ import os
 @click.command()
 @click.argument("filename", required=True)
 @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
-def cli(filename, c):
+@click.option("-l", is_flag=True, help="Count the number of lines in a file")
+def cli(filename, c, l):
     if c:
         cur_dir = os.getcwd()
         cur_path = os.path.join(cur_dir, filename)
         num_bytes = os.path.getsize(cur_path)
         click.echo(f"{num_bytes} {filename}")
+    elif l:
+        with open(filename) as file:
+            num_lines = 0
+            for line in file:
+                num_lines += 1
+        click.echo(f"{num_lines} {filename}")
 
 if __name__ == "__main__":
     cli()
