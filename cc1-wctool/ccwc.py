7ec4e2b64d5c1ce3c29b67ef89c539b456253660
Oliver Then <oli171dr@gmail.com>
2024-11-12 18:20:00 -0500

    Added optional -m flag that now shows amount of chars in file

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -6,7 +6,9 @@ import os
 @click.argument("filename", required=True)
 @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
 @click.option("-l", is_flag=True, help="Count the number of lines in a file")
-def cli(filename, c, l):
+@click.option("-w", is_flag=True, help="Count the number of words in a file")
+@click.option("-m", is_flag=True, help="Count the number of chars in a file")
+def cli(filename, c, l, w, m):
     if c:
         cur_dir = os.getcwd()
         cur_path = os.path.join(cur_dir, filename)
@@ -18,6 +20,33 @@ def cli(filename, c, l):
             for line in file:
                 num_lines += 1
         click.echo(f"{num_lines} {filename}")
+    # elif w:
+    #     with open(filename) as file:
+    #         num_words = 0
+    #         for line in file:
+    #             in_word = False
+    #             for char in line:
+    #                 if char.isalpha():
+    #                     in_word = True
+    #                     continue
+    #                 else:
+    #                     if in_word:
+    #                         num_words += 1
+    #                     in_word = False
+
+                # split_words = line.split(' ')
+                # num_words += len(split_words)
+        click.echo(f"{num_words} {filename}")
+    elif m:
+        with open(filename) as file:
+            num_chars = 0
+            for line in file:
+                for char in line:
+                    num_chars += 1
+                # Account for new line character
+                num_chars += 1
+
+        click.echo(f"{num_chars} {filename}")
 
 if __name__ == "__main__":
     cli()
