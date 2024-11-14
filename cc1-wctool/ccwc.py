3314cfd0dc6e4b9c761e2e138d288809da819208
Oliver Then <oli171dr@gmail.com>
2024-11-14 11:16:32 -0500

    WC app is now able to support default option in which no options are provided - which is equivalent to -c, -l, and -w options

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -8,35 +8,54 @@ import os
 @click.option("-l", is_flag=True, help="Count the number of lines in a file")
 @click.option("-w", is_flag=True, help="Count the number of words in a file")
 @click.option("-m", is_flag=True, help="Count the number of chars in a file")
-def cli(filename, c, l, w, m):
+def cli(filename, c, l, w, m):        
     if c:
-        cur_dir = os.getcwd()
-        cur_path = os.path.join(cur_dir, filename)
-        num_bytes = os.path.getsize(cur_path)
+        num_bytes = count_bytes(filename)
         click.echo(f"{num_bytes} {filename}")
     elif l:
-        with open(filename) as file:
-            num_lines = 0
-            for line in file:
-                num_lines += 1
+        num_lines = count_lines(filename)
         click.echo(f"{num_lines} {filename}")
     elif w:
-        with open(filename) as file:
-            num_words = 0
-            for line in file:
-                split_words = line.split()
-                num_words += len(split_words)
+        num_words = count_words(filename)
         click.echo(f"{num_words} {filename}")
     elif m:
-        with open(filename) as file:
-            num_chars = 0
-            for line in file:
-                for char in line:
-                    num_chars += 1
-                # Account for new line character
-                num_chars += 1
-
+        num_chars = count_chars(filename)
         click.echo(f"{num_chars} {filename}")
+    else:
+        num_bytes = count_bytes(filename)
+        num_lines = count_lines(filename)
+        num_words = count_words(filename)
+        click.echo(f"{num_lines} {num_words} {num_bytes} {filename}")
+
+def count_bytes(filename):
+    cur_dir = os.getcwd()
+    cur_path = os.path.join(cur_dir, filename)
+    return os.path.getsize(cur_path)
+
+def count_lines(filename):
+    with open(filename) as file:
+        num_lines = 0
+        for line in file:
+            num_lines += 1
+    return num_lines
+
+def count_words(filename): 
+    with open(filename) as file:
+        num_words = 0
+        for line in file:
+            split_words = line.split()
+            num_words += len(split_words)
+    return num_words
+
+def count_chars(filename):
+    with open(filename) as file:
+        num_chars = 0
+        for line in file:
+            for char in line:
+                num_chars += 1
+            # Account for new line character
+            num_chars += 1
+    return num_chars
 
 if __name__ == "__main__":
     cli()
