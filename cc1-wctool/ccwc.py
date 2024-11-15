7c4a97099d28a05eb0022a137ac7d94041ace4a9
Oliver Then <oli171dr@gmail.com>
2024-11-15 17:31:21 -0500

    Refactored ccwc app to treat file argument as file object instead of a string

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -3,59 +3,66 @@ import os
 
 
 @click.command()
-@click.argument("filename", required=True)
+@click.argument("file", type=click.File())
 @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
 @click.option("-l", is_flag=True, help="Count the number of lines in a file")
 @click.option("-w", is_flag=True, help="Count the number of words in a file")
 @click.option("-m", is_flag=True, help="Count the number of chars in a file")
-def cli(filename, c, l, w, m):        
+def cli(file, c, l, w, m):        
     if c:
-        num_bytes = count_bytes(filename)
-        click.echo(f"{num_bytes} {filename}")
+        num_bytes = count_bytes(file)
+        click.echo(f"{num_bytes} {file.name}")
     elif l:
-        num_lines = count_lines(filename)
-        click.echo(f"{num_lines} {filename}")
+        num_lines = count_lines(file)
+        click.echo(f"{num_lines} {file.name}")
     elif w:
-        num_words = count_words(filename)
-        click.echo(f"{num_words} {filename}")
+        num_words = count_words(file)
+        click.echo(f"{num_words} {file.name}")
     elif m:
-        num_chars = count_chars(filename)
-        click.echo(f"{num_chars} {filename}")
+        num_chars = count_chars(file)
+        click.echo(f"{num_chars} {file.name}")
     else:
-        num_bytes = count_bytes(filename)
-        num_lines = count_lines(filename)
-        num_words = count_words(filename)
-        click.echo(f"{num_lines} {num_words} {num_bytes} {filename}")
+        num_bytes = count_bytes(file)
+        num_lines = count_lines(file)
+        num_words = count_words(file)
+        click.echo(f"{num_lines} {num_words} {num_bytes} {file.name}")
 
-def count_bytes(filename):
-    cur_dir = os.getcwd()
-    cur_path = os.path.join(cur_dir, filename)
-    return os.path.getsize(cur_path)
 
-def count_lines(filename):
-    with open(filename) as file:
-        num_lines = 0
-        for line in file:
-            num_lines += 1
+def count_bytes(file):
+    # cur_dir = os.getcwd()
+    # cur_path = os.path.join(cur_dir, file)
+    # return os.path.getsize(cur_path)
+    file.seek(0, os.SEEK_END)
+    return file.tell()
+
+
+def count_lines(file):
+    file_lines = file.readlines()
+    num_lines = 0
+    for line in file_lines:
+        num_lines += 1
     return num_lines
 
-def count_words(filename): 
-    with open(filename) as file:
-        num_words = 0
-        for line in file:
-            split_words = line.split()
-            num_words += len(split_words)
+
+def count_words(file):
+    file_lines = file.readlines()
+    num_words = 0
+    for line in file_lines:
+        split_words = line.split()
+        num_words += len(split_words)
     return num_words
 
-def count_chars(filename):
-    with open(filename) as file:
-        num_chars = 0
-        for line in file:
-            for char in line:
-                num_chars += 1
-            # Account for new line character
+
+def count_chars(file):
+    file_lines = file.readlines()
+    num_chars = 0
+    for line in file_lines:
+        for char in line:
             num_chars += 1
+        # Account for new line character
+        num_chars += 1
     return num_chars
+
 
 if __name__ == "__main__":
     cli()
