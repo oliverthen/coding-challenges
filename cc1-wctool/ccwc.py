f536a6b075a8fe6b845bc673c1365b911e8bf67e
Oliver Then <oli171dr@gmail.com>
2024-11-16 17:32:08 -0500

    Now able to use ccwc tool with stdin

--- cc1-wctool/ccwc.py
+++ cc1-wctool/ccwc.py
@@ -1,42 +1,58 @@
 import click
 import os
+import sys
+from io import StringIO, BytesIO
 
 
 @click.command()
-@click.argument("file", type=click.File())
+@click.argument("file", type=click.File(), default=sys.stdin)
 @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
 @click.option("-l", is_flag=True, help="Count the number of lines in a file")
 @click.option("-w", is_flag=True, help="Count the number of words in a file")
 @click.option("-m", is_flag=True, help="Count the number of chars in a file")
-def cli(file, c, l, w, m):        
+def cli(file, c, l, w, m):
+    if file != sys.stdin:
+        file_name = file.name
+        std_input = False
+    else:
+        file_name = ''
+        std_input = True
+        file = StringIO(file.read())
+
     if c:
-        num_bytes = count_bytes(file)
-        click.echo(f"{num_bytes} {file.name}")
+        num_bytes = count_bytes(file, std_input)
+        click.echo(f"{num_bytes} {file_name}")
     elif l:
         num_lines = count_lines(file)
-        click.echo(f"{num_lines} {file.name}")
+        click.echo(f"{num_lines} {file_name}")
     elif w:
         num_words = count_words(file)
-        click.echo(f"{num_words} {file.name}")
+        click.echo(f"{num_words} {file_name}")
     elif m:
-        num_chars = count_chars(file)
-        click.echo(f"{num_chars} {file.name}")
+        num_chars = count_chars(file, std_input)
+        click.echo(f"{num_chars} {file_name}")
     else:
-        num_bytes = count_bytes(file)
         num_lines = count_lines(file)
         num_words = count_words(file)
-        click.echo(f"{num_lines} {num_words} {num_bytes} {file.name}")
+        num_bytes = count_bytes(file, std_input)
+
+        click.echo(f"{num_lines} {num_words} {num_bytes} {file_name}")
 
 
-def count_bytes(file):
-    # cur_dir = os.getcwd()
-    # cur_path = os.path.join(cur_dir, file)
-    # return os.path.getsize(cur_path)
-    file.seek(0, os.SEEK_END)
-    return file.tell()
+def count_bytes(file, std_input):
+    if not std_input:
+        file.seek(0, os.SEEK_END)
+        return file.tell()
+    else:
+        # Need to convert StringIO buffer to BytesIO Buffer to count bytes
+        data = file.getvalue()
+        byte_data = data.encode('utf-8')
+        buffer = BytesIO(byte_data)
+        return len(buffer.read())
 
 
 def count_lines(file):
+    file.seek(0)
     file_lines = file.readlines()
     num_lines = 0
     for line in file_lines:
@@ -45,6 +61,7 @@ def count_lines(file):
 
 
 def count_words(file):
+    file.seek(0)
     file_lines = file.readlines()
     num_words = 0
     for line in file_lines:
@@ -53,15 +70,15 @@ def count_words(file):
     return num_words
 
 
-def count_chars(file):
-    file_lines = file.readlines()
-    num_chars = 0
-    for line in file_lines:
-        for char in line:
-            num_chars += 1
-        # Account for new line character
-        num_chars += 1
-    return num_chars
+def count_chars(file, std_input):
+    file.seek(0)
+    if not std_input:
+        data = file.read()
+        # Need to add count of lines as above line for regular file does not count newline char
+        return len(data) + count_lines(file)
+    else:
+        data = file.read()
+        return len(data)
 
 
 if __name__ == "__main__":
