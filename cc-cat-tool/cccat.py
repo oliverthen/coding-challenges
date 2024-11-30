5d761e7048607c671442784eff2dd6d36edddd51
Oliver Then <oli171dr@gmail.com>
2024-11-30 17:28:38 -0500

    Added functionality to cccat program to print contents of a file to standard out

--- cc-cat-tool/cccat.py
+++ cc-cat-tool/cccat.py
@@ -1,11 +1,14 @@
 import click
+import sys
 
 
 @click.command()
-# @click.argument("file", type=click.File(), default=sys.stdin)
+@click.argument("file", type=click.File())
 # @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
-def cli():
-   click.echo("Hello")
+def cli(file):
+#    if file is None or file == '-':
+#     click.echo(file)   
+   click.echo(file.read())
 
 if __name__ == "__main__":
     cli()
