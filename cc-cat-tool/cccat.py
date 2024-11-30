28abc04ecf5e194d3d05625cdf21ee4a208d8f5f
Oliver Then <oli171dr@gmail.com>
2024-11-30 17:40:44 -0500

    Added functionality in cccat program to concantenante files

--- cc-cat-tool/cccat.py
+++ cc-cat-tool/cccat.py
@@ -3,12 +3,11 @@ import sys
 
 
 @click.command()
-@click.argument("file", type=click.File())
+@click.argument("files", type=click.File(), nargs=-1)
 # @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
-def cli(file):
-#    if file is None or file == '-':
-#     click.echo(file)   
-   click.echo(file.read())
+def cli(files):
+   for file in files:
+    click.echo(file.read())
 
 if __name__ == "__main__":
     cli()
