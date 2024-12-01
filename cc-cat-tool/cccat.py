ea811ea736a23714d180efa29be3700977fe3ee4
Oliver Then <oli171dr@gmail.com>
2024-12-01 09:37:52 -0500

    Added functionality to cccat program by allowing -n flag to number lines as they are printed out

--- cc-cat-tool/cccat.py
+++ cc-cat-tool/cccat.py
@@ -1,13 +1,22 @@
 import click
-import sys
 
 
 @click.command()
 @click.argument("files", type=click.File(), nargs=-1)
-# @click.option("-c", is_flag=True, help="Count the number of bytes in a file")
-def cli(files):
+@click.option("-n", is_flag=True, help="Number the lines as they are printed out")
+def cli(files, n):   
    for file in files:
-    click.echo(file.read())
+    file_lines = file.readlines()
+       
+    for count, line in enumerate(file_lines, 1):
+       # Remove new line character
+       formatted_line = line.split("\n")[0]
+
+       if not n:
+        click.echo(formatted_line)
+        continue 
+
+       click.echo(f"{count} {formatted_line}")
 
 if __name__ == "__main__":
     cli()
