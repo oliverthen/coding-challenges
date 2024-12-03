5cf309ba922dab7af95de95a24503ad2fdd29c3f
Oliver Then <oli171dr@gmail.com>
2024-12-03 17:32:24 -0500

    Now able to number lines, both including and excluding non-blank lines

--- cc-cat-tool/cccat.py
+++ cc-cat-tool/cccat.py
@@ -1,22 +1,46 @@
 import click
-
+import sys
+from io import StringIO
 
 @click.command()
 @click.argument("files", type=click.File(), nargs=-1)
-@click.option("-n", is_flag=True, help="Number the lines as they are printed out")
-def cli(files, n):   
-   for file in files:
-    file_lines = file.readlines()
-       
-    for count, line in enumerate(file_lines, 1):
-       # Remove new line character
-       formatted_line = line.split("\n")[0]
+@click.option("-n", is_flag=True, help="Number the lines as they are printed out including blank lines")
+@click.option("-b", is_flag=True, help="Number the lines as they are printed out not including blank lines")
+def cli(files, n, b):
+    file_lines = []
+    if len(files) == 0:
+        # Convert stdin to a readable file and insert it in one item tuple
+        files = (StringIO(sys.stdin.read()),)
+    for file in files:
+        file_lines.extend(file.readlines())
 
-       if not n:
-        click.echo(formatted_line)
-        continue 
+    count = 1
 
-       click.echo(f"{count} {formatted_line}")
+    for line in file_lines:
+        formatted_line = line.split("\n")[0]
+
+        if n:
+            click.echo(f"{count} {formatted_line}")
+            count += 1
+        elif b:
+            if formatted_line == '':
+                click.echo(formatted_line)
+            else:
+                click.echo(f"{count} {formatted_line}")
+                count += 1
+        else:
+            click.echo(formatted_line)
+
+
+    # for count, line in enumerate(file_lines, 1):
+    #     # Remove new line character
+    #     formatted_line = line.split("\n")[0]
+
+    #     if not n:
+    #         click.echo(formatted_line)
+    #         continue 
+
+    #     click.echo(f"{count} {formatted_line}")
 
 if __name__ == "__main__":
     cli()
