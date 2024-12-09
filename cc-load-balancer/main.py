a41191c8e73a5941e8cddf1efb9aba1e42462d73
Oliver Then <oli171dr@gmail.com>
2024-12-09 18:12:30 -0500

    Start of creating a load balancer

---
+++ cc-load-balancer/main.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostname = 'localhost'
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+
+
+if __name__ == '__main__':
+    
+
